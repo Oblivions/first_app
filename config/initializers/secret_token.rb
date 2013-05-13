7db3a3709bad9625a9b03658a2701ea14e960b4e
Jesus Manuel Grado Valles <jgrado691103@gmail.com>
2013-05-13 08:51:50 -0600

    Almacenamiento Inicial e Inizializacion del Proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '0bc18ec22e9fc1bdd0020b347e1bc2ab057a843f9d51b85ac49d7d0862d3903826c7c868f2e9a8e05d3891317ce234e6000503223b9ae6d0c4e84f31ab30decf'
